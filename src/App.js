005dcaeba8ca7fec645bb0e335af28b134d0bf4c
asai-nomoto <aji_nomoto-8@yahoo.co.jp>
2022-11-13 15:12:23 +0900

    useStateを使用して書き換え

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+// import * as React from 'react'
 import './App.css'
+import React, { useState } from 'react'
 
 /**
  *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setdogUrl] = useState(
+    'https://images.dog.ceo/breeds/mountain-bernese/n02107683_5699.jpg',
+  )
   return (
     <>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img
-        src="https://images.dog.ceo/breeds/mountain-bernese/n02107683_5699.jpg"
-        alt="いぬの画像"
-      />
+      <img src={dogUrl} alt="いぬの画像" />
     </>
   )
 }
